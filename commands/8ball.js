47f866f9f3ea84c913bb6001020c062e47e7647f
Yikey <gtc122004@gmail.com>
2018-03-12 22:51:46 -0400

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -4,6 +4,7 @@ module.exports.run = async (bot, message, args) => {
 			 " ask again later.", " outlook not so good, retry.", " ask again later."];
 	var randomchoice = Math.floor(Math.random() * responses.length);
 	message.reply("The 8ball says... "+"**"+responses[randomchoice]+"**");
+	// credits to gt_c for making the command :D
 }
 module.exports.help = {
 	name: "8ball"
